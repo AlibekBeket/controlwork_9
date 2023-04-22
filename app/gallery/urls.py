3f01e95dd534db89d9f15910c57d591893e30413
Alibek Beket <alibek1beket@gmail.com>
2023-04-22 14:39:58 +0600

    #3 Страница создания
    Создал функцию для создания фотографии и указал его в url

--- app/gallery/urls.py
+++ app/gallery/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from gallery.views.gallery import GalleryListView
+from gallery.views.gallery import GalleryListView, PhotoAddView
 
 urlpatterns = [
     path('', GalleryListView.as_view()),
     path('gallery/', GalleryListView.as_view(), name='gallery_list'),
+    path('photo_create/', PhotoAddView.as_view(), name='photo_create'),
 ]
--- app/gallery/views/gallery.py
+++ app/gallery/views/gallery.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views.generic import ListView, CreateView
 
 from gallery.models import Photo
+
+from gallery.forms import PhotoForm
 
 
 class GalleryListView(ListView):
@@ -8,3 +12,23 @@ class GalleryListView(ListView):
     model = Photo
     context_object_name = 'photos'
     ordering = ('created_at',)
+
+
+class PhotoAddView(CreateView):
+    template_name = 'photo_create.html'
+    model = Photo
+    form_class = PhotoForm
+
+    def get_success_url(self):
+        return reverse('gallery_list')
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            user = self.request.user
+            signature = form.cleaned_data.get('signature')
+            photo = form.cleaned_data.get('photo')
+            Photo.objects.create(author=user, signature=signature, photo=photo)
+            return redirect(reverse('gallery_list'))
+        return render(request, 'photo_create.html',
+                      context={'form': form})
